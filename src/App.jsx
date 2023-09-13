8664d473d31166f04ed15f0ed81e60305ae09e31
programmarreza <programarreza@gmail.com>
2023-09-14 01:55:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Cart from './component/Carts/Carts'
+import Home from './component/Homes/Homes'
+
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='text-4xl text-center'>Actor app</h1>
+      <Home></Home>
+      <Cart></Cart>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Homes/Homes.jsx
@@ -0,0 +1,10 @@
+import './Homes.css'
+const Homes = () => {
+	return (
+		<div>
+			<h1>Homes</h1>
+		</div>
+	);
+};
+
+export default Homes;
